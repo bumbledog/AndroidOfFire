53b9adec8b1a98f913e1ce92bd2125b4b6a0aa6d
Ian Pougher <ianpougher@gmail.com>
2018-03-26 14:03:51 -0600

    adds ionicon support for the app

--- src/components/App.js
+++ src/components/App.js
@@ -11,33 +11,16 @@ import {
   Text,
   View
 } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <RootTab/>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -55,4 +38,5 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  
 });
